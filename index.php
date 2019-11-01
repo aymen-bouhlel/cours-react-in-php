06dd40313c2410ade28506778a34b197bbbb7710
Bouhlel El Bechri <bouhlel_elbechri@web.de>
2019-11-01 13:06:46 +0100

    Load React

--- index.php
+++ index.php
@@ -33,5 +33,9 @@ $articles = App\Generator::getArticles();
             <?php endforeach ?>
         </div>
     </div>
+    <!-- Load React. -->
+    <!-- Note: when deploying, replace "development.js" with "production.min.js". -->
+    <script src="https://unpkg.com/react@16/umd/react.development.js" crossorigin></script>
+    <script src="https://unpkg.com/react-dom@16/umd/react-dom.development.js" crossorigin></script>
 </body>
 </html>
